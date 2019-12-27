f73728cb092e1b339e8dd9d80137b0857dad954a
Buanzo <buanzo@buanzo.com.ar>
2019-12-27 12:17:03 -0300

    bumped release to 0.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 # sets setup up :P
 setup(name='openpgp-requests',
-      version='0.4',
+      version='0.5',
       description='A wrapper to the requests module adding OpenPGP support.',
       url='https://github.com/buanzo/openpgp-requests',
       author='Arturo "Buanzo" Busleiman',
